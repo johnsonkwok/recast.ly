5033f435f0d55de7167d466869f9597529f922c1
johnsonkwok <johnsonkwok1@gmail.com>
2018-11-27 12:08:47 -0800

    Begin implementing searchYoutube functionality

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,37 @@
+// import of the key? 
+import YOUTUBE_API_KEY from '../lib/searchYouTube.js';
+
+// searchYouTube({ key: 'API_KEY', query: 'cats', max: 10 }, () => {});
+
 var searchYouTube = (options, callback) => {
-  // TODO
+
+// var options = {
+//   key: 'AIzaSyCU7TiHAESHvnDkPA8U2wIbsi5v1TVKOWk',
+//   query: 'react',
+//   max: 5
+// };
+
+  $.get('https://www.googleapis.com/youtube/v3/search', {
+    'maxResults': options.max,
+    'part': 'snippet',
+    'q': options.query,
+    'key': options.key,
+    'type': 'video',
+  }).done(callback);
+
+  // $.ajax({
+  //   url:'https://www.googleapis.com/youtube/v3/search', 
+  //   type: 'GET',
+  //   data: {
+  //     'maxResults': options.max,
+  //     'part': 'snippet',
+  //     'q': options.query,
+  //     'key': options.key,
+  //     'type': 'video',
+  //   }, 
+  //   // success: function(data) { console.log(data)}, 
+  // }).done(callback);
+
 };
 
 export default searchYouTube;
